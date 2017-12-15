353d739c04bc3fa51bd68f46b8b11381ec30df9f
Hugo Silva <hugo.f.v.silva@gmail.com>
2017-12-15 00:04:29 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import setup, find_packages
+
+
+def read_file(filename):
+    """Read a file into a string"""
+    path = os.path.abspath(os.path.dirname(__file__))
+    filepath = os.path.join(path, filename)
+    try:
+        return open(filepath).read()
+    except IOError:
+        return ''
+
+
+setup(
+    name='django-zxcvbn',
+    version='1.0',
+    author='Hugo Vieira Da Silva',
+    author_email='hugo.fvsilva@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/hugofvs/django-zxcvbn',
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Django',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
+    long_description=read_file('README.md'),
+    test_suite="runtests.runtests",
+    zip_safe=False,
+)
